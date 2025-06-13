db43e8956290b7c0bd6332c8f9d6b786caaba004
Vaanya3 <Pajerosfx2.8@gmail.com>
2025-06-13 22:21:32 +0530

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AdvancedTradingAnalyzer from './ai-trading-analyzer'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AdvancedTradingAnalyzer />
+  </React.StrictMode>
+)
